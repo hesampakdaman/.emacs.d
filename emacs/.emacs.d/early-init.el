0d911bb9dc1eb72fd065f1d8122b3a2bfe99a8da
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2023-07-13 10:57:53 +0200

    [NEW] Added package and compilation settings to early-init

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -2,6 +2,15 @@
 ;; startup
 (setq gc-cons-threshold (* 100 (expt 10 6)))
 
+;; package
+(require 'package)
+(setq package-native-compile t)
+(setq package-check-signature nil)
+(setq package-quickstart t)
+
+;; compilations
+(setq native-comp-deferred-compilation t)
+
 ;; Lower garbage collection threshold to 1MB after startup to minimize
 ;; impact of GC during normal use
 (add-hook 'after-init-hook
