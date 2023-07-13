a86b9658913c94415899aea9b97c1aa1367e900e
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2023-07-13 13:30:59 +0200

    [FIX] Only refresh, fetch packages when 'use-package not installed

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,13 +1,15 @@
-(require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-
-(require 'use-package-ensure)
-(setq use-package-always-ensure t)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package)
+  (require 'use-package-ensure)
+  (setq use-package-always-ensure t))
 
 ;; follow symlinks without prompt
 (setq vc-follow-symlinks t)
 
 ;; load emacs settings
+(require 'use-package)
 (require 'org)
 (org-babel-load-file
  (expand-file-name "start.org" user-emacs-directory))
