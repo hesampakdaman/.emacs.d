6534359315981e40416445d7574f5959bc527a8c
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2023-08-01 20:42:39 +0200

    [REFACTOR] Cleans up init.el: use-package and require org

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,15 +1,8 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package)
-  (require 'use-package-ensure)
-  (setq use-package-always-ensure t))
 
 ;; follow symlinks without prompt
 (setq vc-follow-symlinks t)
 
 ;; load emacs settings
-(require 'use-package)
-(require 'org)
 (org-babel-load-file
  (expand-file-name "start.org" user-emacs-directory))
