917301bb42c8ebafaf511e2a219d96966a94acf4
Hesam Pakdaman <hesam.pakdaman@creatorstudio.com>
2023-08-18 12:41:21 +0200

    [NEW] Collect all org configs into init.org

--- emacs/.emacs.d/init.el
+++
@@ -1,9 +0,0 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-
-;; follow symlinks without prompt
-(setq vc-follow-symlinks t)
-
-;; load emacs settings
-(org-babel-load-file
- (expand-file-name "start.org" user-emacs-directory))
--- emacs/.emacs.d/macros.el
+++
@@ -1,2 +0,0 @@
-(fset 'macro/projectile
-   (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([3 112 120 115 134217848 116 114 97 110 115 112 111 115 101 102 114 97 109 101 return 8388715 8388723 8388723 24 43 21 50 53 24 94 8388716 3 112 118 8388726 8388714 3 112 68 8388712 8388712] 0 "%d")) arg)))
